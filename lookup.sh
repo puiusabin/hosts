9b4b1679d4826760f9a79302facf6779e661ff57
puiusabin <puiumihaisabin@gmail.com>
2024-11-18 16:32:14 +0200

    al doilea commit

--- lookup.sh
+++ lookup.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
+validate_host_ip() {
+    local hostname="$1"
+    local expected_ip="$2"
+    local dns_server="${3:-}"
+
+    local nslookup_cmd="nslookup $hostname"
+    if [[ -n "$dns_server" ]]; then
+        nslookup_cmd+=" $dns_server"
+    fi
+
+    local current_ip=$(eval "$nslookup_cmd" 2>/dev/null | awk '/^Address: / { print $2 }' | head -n 1)
+
+    if [[ -z "$current_ip" ]]; then
+        return 1
+    fi
+
+    if [[ "$expected_ip" != "$current_ip" ]]; then
+        echo "Bogus IP for $hostname in /etc/hosts !"
+        return 1
+    fi
+
+    return 0
+}
+
 cat /etc/hosts | while read -r ip name; do
     if [[ -z "$ip" || "$ip" =~ ^# ]]; then
         continue
     fi
 
-    current_ip=$(nslookup "$name" 2>/dev/null | awk '/^Address: / { print $2 }' | head -n 1)
-
-    if [[ -z "$current_ip" ]]; then
-        continue
-    fi
-
-    if [[ "$ip" != "$current_ip" ]]; then
-        echo "Bogus IP for $name in /etc/hosts !"
-    fi
+    validate_host_ip "$name" "$ip"
 done
-
